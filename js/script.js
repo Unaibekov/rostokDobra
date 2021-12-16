4a279c41d275a8d3ea81e4b6b5b8faf5c0ae6acd
unajbekov <i.unaybekov@zenit.ru>
2021-12-16 16:15:23 +0300

    menu

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
-const faqs = document.querySelectorAll('.faq'); 
+const faq = document.querySelector('.faq'); 
 
-faqs.forEach((faq) => {
     faq.addEventListener('click', () => {
         faq.classList.toggle('active');
     });
-});
+
+const menuButton = document.querySelector('.menu-button');
+const menu = document.querySelector('.nav-menu');
+const menuClose = document.querySelector('.menu-close')
+
+    menuButton.addEventListener('click', () => {
+        menu.classList.add('is-active');
+        menuClose.classList.add('is-active');
+    });
+    menuClose.addEventListener('click', () => {
+        menu.classList.remove('is-active');
+        menuClose.classList.remove('is-active');
+    });
